09ae0c5d6341438a762098145d55c3c9dc31fbc1
tedoaba <tedoaba@gmail.com>
2024-09-03 10:45:45 +0300

    Sentiment Analysis

--- src/sentiment_analysis.py
+++ src/sentiment_analysis.py
@@ -1,5 +1,12 @@
+import pandas as pd
 from textblob import TextBlob
 
 def analyze_sentiment(text):
     analysis = TextBlob(text)
     return analysis.sentiment.polarity
+
+def aggregate_sentiments_by_date(df, date_column='date', sentiment_column='sentiment', output_column='Average Sentiment'):
+    daily_sentiment = df.groupby(date_column)[sentiment_column].mean().reset_index()
+    daily_sentiment.columns = [date_column, output_column]
+    return daily_sentiment
+
