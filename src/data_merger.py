b3c29c0ebc02ff0d9d810db83c2a98451e94622f
tedoaba <tedoaba@gmail.com>
2024-09-03 10:46:08 +0300

    Data Merger

--- src/data_merger.py
+++ src/data_merger.py
@@ -3,3 +3,7 @@ import pandas as pd
 def merge_data(news_df, stock_df, on='date'):
     merged_df = pd.merge(news_df, stock_df, on=on)
     return merged_df
+
+def merge_sentiment_with_stock(stock_df, sentiment_df):
+    daily_df = pd.merge(stock_df[['date', 'Daily Return']], sentiment_df, on='date', how='inner')
+    return daily_df
